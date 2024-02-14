bf18cfefd8daefd1626939eeec1d6dcf6050d461
tanaka <152840606+kraw-t@users.noreply.github.com>
2024-02-14 16:55:06 +0900

    report: add report "fuji" in 04-challenge01

--- 04_testing_and_cicd/01_testing_and_validation/main.cpp
+++ 04_testing_and_cicd/01_testing_and_validation/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 /*----これらの関数を変更する必要はありません----*/
 
@@ -40,18 +41,164 @@ std::string greeting(const std::string& name, double num) {
 /*----難易度: 富士----*/
 
 // `my_assert` をここに定義し、以降のテストに使用してください。
+// 1. `expr` はブール値として評価される式です。
+// 2. `msg` はオプションのメッセージであり、`expr` が `false` と評価された場合に例外にスローされます。
+void my_assert(bool expr, std::string msg) {
+    if (!expr) {
+        throw std::logic_error(msg);
+    }
+    return;
+}
 
 // `contains` 用のテスト `test_contains` を作成してください
+bool test_contains() {
+    bool ret {true};
+    const std::vector<std::string> inputs_names {"Nick", "Mike", "Lewis", "Takaaki", "Nikos", "Nickos"};
+    const std::vector<bool> expected_returns {true, false, true, false, true, false};
+
+    // テスト入力と期待出力のサイズが一致していない場合は失敗扱いとする
+    try {
+        my_assert(inputs_names.size() == expected_returns.size(), "the size of input/output are different.");
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error in test_contains: " << e.what() << std::endl;
+        return false;
+    }
+
+    bool output {};
+    for (int i = 0; i < inputs_names.size(); i++) {
+        output = contains(inputs_names[i], names);
+        try {
+            my_assert(output == expected_returns[i], "expected: " + std::to_string(expected_returns[i]) + ", but actual: " + std::to_string(output) + ".");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in contains: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+
+    return ret;
+}
 
 // `get_name` 用のテスト `test_get_name` を作成してください
+bool test_get_name(void) {
+    bool ret {true};
+    const std::vector<std::vector<std::string>> inputs_and_expected_returns {
+        // テスト入力, 期待出力 の順
+        {"Nick", "Nick"},
+        {"Mike", ""},
+        {"Lewis", "Lewis"},
+        {"Takaaki", ""},
+        {"Nikos", "Nikos"},
+        {"Nickos", ""}
+    };
+
+    std::string output {};
+    for (const auto& test : inputs_and_expected_returns) {
+        output = get_name(test[0], names);
+        try {
+            my_assert(output == test[1], "expected: " + test[1] + ", but actual: " + output + ".");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in get_name: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+    return ret;
+}
 
 // `add_name` 用のテスト `test_add_name` を作成してください
+bool test_add_name(void) {
+    bool ret {true};
+    int count {0};
+    std::vector<std::string> name_vec;
+
+    for (const auto& name : names) {
+        add_name(name, name_vec);
+        count++;
+        try {
+            my_assert(name_vec[count - 1] == name, "expected: " + name + ", but actual: " + name_vec[count - 1] + ".");
+            my_assert(name_vec.size() == count, "expected size is " + std::to_string(count) + " but actual size is " + std::to_string(name_vec.size()) + ".");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in add_name: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+    return ret;
+}
 
 // `add_two` 用のテスト `test_add_two` を作成してください
+bool test_add_two(void) {
+    bool ret {true};
+    const std::vector<std::vector<int>> inputs_and_expected_returns {
+        // テスト入力, 期待出力 の順
+        {1, 3},
+        {3, 5},
+        {-2, 0},
+        {-10, -8}
+    };
+
+    int output {};
+    for (const auto& test : inputs_and_expected_returns) {
+        output = add_two(test[0]);
+        try {
+            my_assert(output == test[1], "expected return is " + std::to_string(test[1]) + " but " + std::to_string(output) + " is returned.");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in add_two: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+    return ret;
+}
 
 // `divide_by_two` 用のテスト `test_divide_by_two` を作成してください
+bool test_divide_by_two(void) {
+    bool ret {true};
+    const std::vector<std::vector<double>> inputs_and_expected_returns {
+        // テスト入力, 期待出力 の順
+        {1.0, 0.5},
+        {35.0, 17.5},
+        {-200.0, -100.0},
+        {0, 0}
+    };
+
+    double output {};
+    for (const auto& test : inputs_and_expected_returns) {
+        output = divide_by_two(test[0]);
+        try {
+            my_assert(output == test[1], "expected return is " + std::to_string(test[1]) + " but " + std::to_string(output) + " is returned.");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in divide_by_two: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+    return ret;
+}
 
 // `greeting` 用のテスト `test_greeting` を作成してください
+bool test_greeting(void) {
+    int ret {true};
+    const std::vector<double> temperatures {3.8, 7.3, 12.6};
+
+    // テスト入力と期待出力のサイズが一致していない場合は失敗扱いとする
+    try {
+        my_assert(names.size() == temperatures.size(), "the size of input/output are different.");
+    } catch (const std::logic_error& e) {
+        std::cerr << "Error in test_contains: " << e.what() << std::endl;
+        return false;
+    }
+
+    std::string output {};
+    std::string expected_msg {};
+    for (int i = 0; i < names.size(); i++) {
+        expected_msg = "Hello, " + names[i] + ". It is " + std::to_string(temperatures[i]) + " degrees warmer today than yesterday";
+        output = greeting(names[i], temperatures[i]);
+        try {
+            my_assert(output == expected_msg, "expected message: \"" + expected_msg + "\"");
+        } catch (const std::logic_error& e) {
+            std::cerr << "Error in greeting: " << e.what() << std::endl;
+            ret = false;
+        }
+    }
+    return ret;
+}
 
 
 /*----難易度: キリマンジャロ----*/
@@ -64,3 +211,22 @@ std::string greeting(const std::string& name, double num) {
 
 // 次の式全体をテストする `test_complex_greeting` を `my_assert` を使用して作成してください。式がエラーになった場合は、エラーの理由がわかるメッセージを `msg` に指定してください。
 // greeting(get_name("Frosty the Snowman", {"Oatmeal", "Prancer", "Rudolph", "Andy"}), divide_by_two(add_two(2)));
+
+
+int main() {
+    int ret {true};
+    ret &= test_contains();
+    ret &= test_get_name();
+    ret &= test_add_name();
+    ret &= test_add_two();
+    ret &= test_divide_by_two();
+    ret &= test_greeting();
+
+    if (ret) {
+        // 全てのretがtrue：エラーなし終了
+        return 0;
+    } else {
+        std::cout << "[FAILED] Some tests ware failed, please check the output logs." << std::endl;
+        return 1;
+    }
+}
